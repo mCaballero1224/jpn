c308995d493b925a8eea6e0d65535a247c6fc64b
mCaballero1224 <mcaballero.dev@gmail.com>
2025-10-30 05:46:17 -0700

    Write intro text

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,6 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
   	<div>
+			<div className="container mx-auto max-w-screen-md">
+				<h2 className="text-center text-3xl py-4">Welcome!</h2>
+				<p className="p-4">This page is a work made to help students taking Oregon State University&apos;s JPN111. The hope is to expand this page in the future to include content involved in other courses in the JPN series. Please feel free to use the resources available here.</p>
+			</div>
 	</div>
   );
 }
