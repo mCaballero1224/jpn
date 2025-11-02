5acbd22ef96d649bab2944e0e5f23cbee1a67e3a
mCaballero1224 <mcaballero.dev@gmail.com>
2025-11-02 09:46:53 -0800

    Add Japanese flag to header

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -1,10 +1,22 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import Flag from '../../public/Flag_of_Japan.svg';
 
 const Header = () => {
 	return (
-		<header className="text-center px-6 py-2 mx-auto">
-			<h1 className="text-red-800 font-bold py-2 text-5xl hover:underline"><Link href="/">JPN111</Link></h1>
-			<p>Learning resources for learning Japanese</p>
+		<header className="max-w-screen-md text-center py-2">
+			<div className="w-full flex justify-center py-2">
+				<Image src={Flag} alt="Japanese Flag"
+					className="mr-5 border-1 shadow-md shadow-red-300"
+					width={120}
+					height={40}
+				/>
+				<h1
+					className="my-auto text-red-800 font-bold text-5xl hover:underline">
+					<Link href="/">JPN111</Link>
+				</h1>
+			</div>
+			<p>Resources for learning Japanese</p>
 		</header>
 	);
 };
