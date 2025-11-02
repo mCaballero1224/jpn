c01046a8f6f93d3d7f2123ebf948d76d2e95177f
mCaballero1224 <mcaballero.dev@gmail.com>
2025-11-02 09:48:05 -0800

    Start work on module pages

--- frontend/app/modules/[slug]/page.tsx
+++ frontend/app/modules/[slug]/page.tsx
@@ -1,12 +1,20 @@
 'use client';
 import { usePathname } from 'next/navigation';
+import LocalNav from '@/components/LocalNav';
 
+const localLinks = [
+	{id: 0, name: 'Vocabulary List', href: '#'},
+	{id: 1, name: 'Conjugation Table', href: '#'},
+];
 
 const ModulePage = () => {
-	const id = usePathname().slice(-1);
+	const id = usePathname().split('/').slice(-1)[0];
 	return (
 		<div>
-			<h2>Module {id}</h2>
+			<h2
+				className="py-2 text-2xl text-center"
+			>Module {id}</h2>
+			<LocalNav localLinks={localLinks} />
 		</div>
 	);
 };
